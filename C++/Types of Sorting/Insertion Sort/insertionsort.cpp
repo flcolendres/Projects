7a13833681ebd9fab7983425fbcd1155b97897fb
Francis Colendres <flcolendres@myseneca.ca>
2022-06-12 15:12:31 -0400

    fixed logic error

--- C++/Types of Sorting/Insertion Sort/insertionsort.cpp
+++ C++/Types of Sorting/Insertion Sort/insertionsort.cpp
@@ -2,16 +2,19 @@
 
 int main() 
 {
+   int i, j;
    int current;
    int arr[10] = { 10, 105, 99, 41, 0, 9000, 812, 23, 1, 34 };
-   for (int i = 0; i < 10; i++)
+   for (i = 0; i < 10; i++)
    {
       current = arr[i];
-      for (int j = i; j > 0 && arr[j - 1] > arr[j]; j--)
+      //for (j = i; j > 0 && arr[j - 1] > current; j--) //ascending
+      for (j = i; j > 0 && arr[j - 1] < current; j--) //descending
       {
          arr[j] = arr[j - 1];
       }
-      arr[i] = current;
+      arr[j] = current;
    }
+   for (int i = 0; i < 10; i++) std::cout << arr[i] << " ";
    return 0;
 }
